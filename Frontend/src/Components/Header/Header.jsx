4452aeb67d898a179788ea97b30e4af0b2d9d6e5
Elias <eliasasfoura7@gmail.com>
2025-06-29 23:13:14 -0300

    Cambio de boton del carrito

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,51 +1,49 @@
-import LogoHeader from "../../assets/LogoHeader.svg"
-import InicioHeader from "../../assets/InicioHeader.svg"
-import ProductosHeader from "../../assets/ProductosHeader.svg"
-import ContactoHeader from "../../assets/ContactoHeader.svg"
-import MiCarritoHeader from "../../assets/MiCarritoHeader.svg"
-import LoginHeader from "../../assets/LoginHeader.svg"
-import "./HeaderStyle.css"
+import LogoHeader from "../../assets/LogoHeader.svg";
+import InicioHeader from "../../assets/InicioHeader.svg";
+import ProductosHeader from "../../assets/ProductosHeader.svg";
+import ContactoHeader from "../../assets/ContactoHeader.svg";
+// import MiCarritoHeader from "../../assets/MiCarritoHeader.svg"; // ya no lo usamos
+import LoginHeader from "../../assets/LoginHeader.svg";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge";
+import "./HeaderStyle.css";
 
-const Header = () => {
+const Header = ({ cartCount = 2 }) => {
+  return (
+    <div className="CajaContainer">
+      <div className="BackgroundHeader" />
+      <img src={LogoHeader} alt="Logo-Header" style={{ position: "absolute" }} />
+      <div className="ContenidoHeader">
+        <input
+          className="BuscadorProductos"
+          type="search"
+          placeholder="Buscar productos.."
+        />
 
-    return (
-        <div className="CajaContainer">
-            <div className="BackgroundHeader" />
-            <img src={LogoHeader} alt="Logo-Header" style={{position: "absolute"}} />
-            <div className="ContenidoHeader">
+        <a href="/inicio">
+          <img src={InicioHeader} alt="" />
+        </a>
 
-                <input className="BuscadorProductos" type="search" name="" id="" placeholder="Buscar productos.."/>
+        <a href="/productos">
+          <img src={ProductosHeader} alt="" />
+        </a>
 
-                
-                    <a href="/inicio">
-                        <img src={InicioHeader} alt="" />
-                    </a>
-                
+        <a href="/contacto">
+          <img src={ContactoHeader} alt="" />
+        </a>
 
-                
-                    <a href="/productos">
-                        <img src={ProductosHeader} alt="" />
-                    </a>
-                
+        <a href="/carrito" style={{ display: "flex", alignItems: "center" }}>
+          <Badge badgeContent={cartCount} color="secondary" overlap="rectangular">
+            <ShoppingCartIcon style={{ fontSize: 30, color: "black" }} />
+          </Badge>
+        </a>
 
-                
-                    <a href="/contacto">
-                        <img src={ContactoHeader} alt="" />
-                    </a>
-                
-
-                
-                    <a href="/carrito">
-                        <img src={MiCarritoHeader} alt="" />
-                    </a>
-
-                    <a href="/login">
-                        <img src={LoginHeader} alt="" />
-                    </a>
-
-            </div>
-        </div>
-    )
-}
+        <a href="/login">
+          <img src={LoginHeader} alt="" />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
